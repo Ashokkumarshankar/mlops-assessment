4c198d6a8ad102457b6cbf1639e640937c17a5a0
Ashokkumarshankar <ashokkumar30495@gmail.com>
2023-08-30 03:26:04 +0200

    checks

--- deploy/sentiment.py
+++ deploy/sentiment.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form, Request
+from fastapi.responses import HTMLResponse
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+
+
+# Load model
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 app = FastAPI()
 
@@ -17,12 +29,7 @@ async def predict(text: dict):
             new_text.append(t)
         return " ".join(new_text)
 
-    MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
-    tokenizer = AutoTokenizer.from_pretrained(MODEL)
-    config = AutoConfig.from_pretrained(MODEL)
-
-    # Load model
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    
 
     text = preprocess(text)
     encoded_input = tokenizer(text, return_tensors='pt')
@@ -40,3 +47,45 @@ async def predict(text: dict):
         results.append({"label": label, "score": score})
 
     return {"results": results}
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.post('/predict')
+async def predict(text: dict):
+    text = text['text']
+    text = preprocess(text)
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    results = []
+
+    for i in range(scores.shape[0]):
+        label = config.id2label[ranking[i]]
+        score = np.round(float(scores[ranking[i]]), 4)
+        results.append({"label": label, "score": score})
+
+    return {"results": results}
+
+@app.get("/test", response_class=HTMLResponse)
+async def test_input(request: Request):
+    return templates.TemplateResponse("test.html", {"request": request})
+
+@app.post("/test")
+async def test_post(text: str = Form(...)):
+    return {"input_text": text}
